427621186040094df156babfde529fd102c8b868
techmsi <sophia.idro@gmail.com>
2020-04-11 16:03:54 -0400

    perf: add offline support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from '/ui/App';
 import '/styles/app.css';
+import * as serviceWorker from './serviceWorker';
 
 const appEl = document.getElementById('root');
 
 render(<App />, appEl);
+
+serviceWorker.register();
